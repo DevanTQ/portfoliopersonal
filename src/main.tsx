2501dce8ca5fe1456039fc8ad65219985d2bc3e0
DvanTQ <devandraelsyadam28@gmail.com>
2026-06-07 23:52:55 +0700

    update scroll

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+if (history.scrollRestoration) {
+  history.scrollRestoration = 'manual'
+}
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
